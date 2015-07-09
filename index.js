e70cb3d99833806564ed6f4ff97ed56f66baa507
Stephen Mathieson <me@stephenmathieson.com>
2015-07-09 15:17:08 -0400

    index: Support mkdirp's "mode" parameter
    This allows directories created with `mkdirp-then` to have specific
    permissions set.

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 var _mkdirp = require('mkdirp')
 var Promise = require('native-or-bluebird')
 
-module.exports = function (dir) {
+module.exports = function (dir, mode) {
   return new Promise(function (resolve, reject) {
-    _mkdirp(dir, function (err) {
+    _mkdirp(dir, mode, function (err) {
       if (err) reject(err)
       else resolve()
     })
--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 
 var fs = require('fs')
 var path = require('path')
+var assert = require('assert')
 
 var mkdirp = require('..')
 
@@ -10,10 +11,22 @@ try {
 } catch (err) {}
 
 describe('mkdirp().then()', function () {
-  it('should work', function () {
+  afterEach(function(){
+    fs.rmdirSync(build)
+  })
+
+  it('should work', function (done) {
     return mkdirp(build).then(function () {
       fs.statSync(build)
-      fs.rmdirSync(build)
+      done()
+    })
+  })
+
+  it('should use "mode"', function (done) {
+    mkdirp(build, 0600).then(function(){
+      var stats = fs.statSync(build)
+      assert.equal(stats.mode & 0777, 0600)
+      done()
     })
   })
 })
